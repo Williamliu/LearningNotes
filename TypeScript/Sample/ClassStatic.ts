54ef5067abd982099d150ee7ff778ca676d5c9f6
William Liu <wliu@OFFICEAD.RADIANT.NET>
2020-03-06 17:47:26 -0800

    Add GoCo Benefits

--- TypeScript/Sample/ClassStatic.ts
+++ TypeScript/Sample/ClassStatic.ts
@@ -1,11 +1,16 @@
 class MyClass {
+    public Age:number = 0;
     public static Total:number=0;
     constructor(n:number) {
         MyClass.Total +=n;   
     }
 
     public show() {
-        console.log(`Static Total: ${MyClass.Total}`);
+        console.log(`Static Total: ${MyClass.Total} Age: ${this.Age}`);
+    }
+
+    public Add() {
+        this.Age++;
     }
 }
 
@@ -14,4 +19,9 @@ mc.show();
 let mc1: MyClass = new MyClass(300);
 mc.show();
 MyClass.Total = 999;
+mc.show();
+console.log('----------------------------')
+setTimeout(mc.Add, 100);
+mc.show();
+setTimeout(mc.show, 200);
 mc.show();
